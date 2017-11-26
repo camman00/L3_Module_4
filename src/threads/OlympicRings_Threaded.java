835bfc145ffa4edba0ac1932bf24f0f51fb41da2
Garrett-Thomas <league@WTS5.attlocal.net>
2017-11-26 12:52:40 -0800

    Created classes for level 3 module 4
    Got back in track

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,7 +1,5 @@
 package threads;
 
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
 
 }
-
