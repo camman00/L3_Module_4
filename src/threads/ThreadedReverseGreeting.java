efa51294dbba62220c5fb7cbad767c233f19771d
camman00 <league@WTS-iMac-11.local>
2017-12-10 12:54:32 -0800

    Stuck on threads
    Stuck

--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -1,8 +1,17 @@
+package threads;
+
+import java.util.HashMap;
 
 public class ThreadedReverseGreeting {
   //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
   //Thread 2 creates Thread 3; and so on, up to Thread 50. 
   //Each thread should print "Hello from Thread <num>!", 
-  //but you should structure your program such that the threads print their greetings in reverse order.
+  //but you should structure your program such that the threads print their greetings in reverse order
+	public static void main(String[] args) {
+		HashMap<Integer,Thread> threads = new HashMap<Integer,Thread>();
+		for(int x = 1; x < 51; x++) {
+			threads.put(x, new Thread(()->{}));
+		}
+	}
 	
 }
